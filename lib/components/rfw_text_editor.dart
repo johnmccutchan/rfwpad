6f64607108e4aa16c7418c683a1a553da3c41c77
John McCutchan <john@johnmccutchan.com>
2024-01-17 22:45:23 -0800

    Initial support for highlighting regions of rfwtext that built a widget under the mouse cursor.

--- lib/components/rfw_text_editor.dart
+++ lib/components/rfw_text_editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 import 'package:rfw/formats.dart';
 import 'package:rfw/rfw.dart';
@@ -13,6 +14,30 @@ class RfwTextEditor extends StatefulWidget {
 
   @override
   State<RfwTextEditor> createState() => _RfwTextEditor();
+}
+
+(int, int) lineCol(SourceLocation l) {
+  final source = l.source;
+  int line = -1;
+  int column = -1;
+  if ((source is String)) {
+    line = 1;
+    column = 0;
+    int offset = 0;
+    for (final ch in source.characters) {
+      if (ch == '\n') {
+        line++;
+        column = 0;
+      } else {
+        column++;
+      }
+      if (offset == l.offset) {
+        return (line, column);
+      }
+      offset++;
+    }
+  }
+  return (line, column);
 }
 
 class _RfwTextEditor extends State<RfwTextEditor> {
@@ -49,6 +74,8 @@ class _RfwTextEditor extends State<RfwTextEditor> {
     );
   ''';
 
+  String _rfwText = _initialRfwText;
+
   final Runtime _runtime = Runtime();
   final DynamicContent _data = DynamicContent();
   final CodeController _codeController = CodeController(
@@ -58,6 +85,8 @@ class _RfwTextEditor extends State<RfwTextEditor> {
 
   Object? _latestRfwError;
 
+  SourceRange? _hoverRange;
+
   @override
   void initState() {
     super.initState();
@@ -65,7 +94,8 @@ class _RfwTextEditor extends State<RfwTextEditor> {
     _runtime
       ..update(coreName, createCoreWidgets())
       ..update(materialName, createMaterialWidgets())
-      ..update(mainName, parseLibraryFile(_initialRfwText));
+      ..update(
+          mainName, parseLibraryFile(_rfwText, sourceIdentifier: _rfwText));
 
     _data
       ..update('greet', <String, Object>{'name': 'World'})
@@ -81,7 +111,9 @@ class _RfwTextEditor extends State<RfwTextEditor> {
   void _onRfwTextChanged(String rfwText) {
     Object? rfwError;
     try {
-      _runtime.update(mainName, parseLibraryFile(rfwText));
+      _runtime.update(
+          mainName, parseLibraryFile(rfwText, sourceIdentifier: rfwText));
+      _rfwText = rfwText;
     } catch (e) {
       rfwError = e;
     } finally {
@@ -93,6 +125,44 @@ class _RfwTextEditor extends State<RfwTextEditor> {
 
   void _onRfwEvent(String name, DynamicMap arguments) {
     print('user triggered event "$name" with data: $arguments');
+  }
+
+  void _onHover(PointerHoverEvent event) {
+    final view = WidgetsFlutterBinding.ensureInitialized().renderViews.first;
+    final hitTestResult = HitTestResult();
+    if (view.hitTest(hitTestResult, position: event.position)) {
+      SourceRange? loc;
+      for (final segment in hitTestResult.path) {
+        final target = segment.target;
+        if (target is RenderObject) {
+          final debugCreator = target.debugCreator;
+          if (debugCreator is DebugCreator) {
+            final element = debugCreator.element;
+            final blob = Runtime.blobNodeFor(element);
+            if (blob != null && blob.source != null) {
+              if (loc == null) {
+                loc = blob.source;
+              } else {
+                // Keep track of the tightest source range we see.
+                int locLength = loc.end.offset - loc.start.offset;
+                int blobLength =
+                    blob.source!.end.offset - blob.source!.start.offset;
+                if (blobLength < locLength) {
+                  loc = blob.source;
+                }
+              }
+            }
+          }
+        }
+      }
+      if (loc != null) {
+        setState(() {
+          _hoverRange = loc;
+          _codeController.selection = TextSelection(
+              baseOffset: loc!.start.offset, extentOffset: loc!.end.offset);
+        });
+      }
+    }
   }
 
   @override
@@ -114,14 +184,16 @@ class _RfwTextEditor extends State<RfwTextEditor> {
         ),
       ),
       Expanded(
-        child: _latestRfwError == null
-            ? RemoteWidget(
-                runtime: _runtime,
-                data: _data,
-                widget: const FullyQualifiedWidgetName(mainName, 'root'),
-                onEvent: _onRfwEvent,
-              )
-            : ErrorWidget(_latestRfwError!),
+        child: MouseRegion(
+            onHover: _onHover,
+            child: _latestRfwError == null
+                ? RemoteWidget(
+                    runtime: _runtime,
+                    data: _data,
+                    widget: const FullyQualifiedWidgetName(mainName, 'root'),
+                    onEvent: _onRfwEvent,
+                  )
+                : ErrorWidget(_latestRfwError!)),
       ),
     ]);
   }
